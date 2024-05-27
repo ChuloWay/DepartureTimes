7f2f46b08e23e2357b5849c9f516d76d4ba57556
victor <okoyeebuka64@gmail.com>
2024-05-27 21:41:01 +0100

    Fix: Updated Port env

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ async function bootstrap() {
 
     app.setViewEngine('ejs');
 
-    await app.listen(3000);
+    const port = process.env.PORT || 3000;
+  await app.listen(port);
 
 }
 bootstrap(); 
